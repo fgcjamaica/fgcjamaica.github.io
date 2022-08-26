ecdfeefddf00b079c237e8708134c863f4907cec
Orville Daley <orvilledaley2004@gmail.com>
2022-08-26 17:24:46 -0500

    feat!: Home Page [dev]

---
+++ src/lib/Header/Header.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Hamburger from '$lib/Header/Hamburger/Hamburger.svelte';
+	import Nav from './Nav/Nav.svelte';
+	import logo from './svelte-logo.svg';
+</script>
+
+<header>
+	<div class="logo-text">FGC Jamaica</div>
+	<Nav />
+</header>
+
+<style lang="scss">
+	header {
+		display: flex;
+		align-items: center;
+		> * {
+			flex: 1;
+		}
+	}
+	.logo-text {
+		font-size: 2.9rem;
+		line-height: 4.8rem;
+		letter-spacing: 0.065em;
+		font-family: var(--font-heading-family);
+		color: black;
+	}
+	.burger {
+		display: none;
+	}
+</style>
